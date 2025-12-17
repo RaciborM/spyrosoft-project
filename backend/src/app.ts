7d7d315a43ce4ff35f3c06e1c853772ddd15ddc5
RaciborM <mraciborski0@gmail.com>
2025-12-17 06:18:04 +0100

    changes un app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
 import powerRouter from './routes/power.routes'
 
 const app = express();
@@ -7,5 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/energy', powerRouter);
+
+const frontendPath = path.join(__dirname, '../frontend/dist');
+app.use(express.static(frontendPath));
+
+app.use((req, res, next) => {
+  if (req.path.startsWith('/api')) return next()
+  res.sendFile(path.join(frontendPath, 'index.html'))
+});
 
 export default app;
